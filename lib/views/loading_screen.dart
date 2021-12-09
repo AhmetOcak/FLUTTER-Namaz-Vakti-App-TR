1421e7ca7c3b2696f60ed5a9b9cf16b4c8a7fe5c
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2021-12-09 21:32:26 +0300

    screens

---
+++ lib/views/loading_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:prayerapp/components/section1.dart';
+import 'package:prayerapp/constants/constant.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({ Key? key }) : super(key: key);
+
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Section1(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
