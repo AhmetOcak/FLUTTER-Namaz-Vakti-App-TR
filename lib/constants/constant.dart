59f8348934b62ca2da74b0bd6edfd83ec5667c16
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2021-12-12 21:15:57 +0300

    color changed

--- lib/constants/constant.dart
+++ lib/constants/constant.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
 
 const Color backgroundColor = Color(0xFF000000);
-const Color accentColor = Color(0xFFC9040E);
-const Color textColor = Color(0xFFFFFFFF);
+const Color accentColor = Color(0xFF04C90E);
